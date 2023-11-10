b817273585382bd8b017de760a6c109c802c1471
bilal ahmed <yoissbilal@gmail.com>
2023-11-10 23:22:49 +0530

    working URL established

--- src/pages/UserLocation.vue
+++ src/pages/UserLocation.vue
@@ -31,7 +31,7 @@
                         </div>
                     </div>
                 </div>
-                <button class="ui button violet">Find CloseBy</button>
+                <button class="ui button violet" @click="FindCloseByButtonPressed">Find CloseBy</button>
             </form>
         </div>
         <div class="ten wide colun segment ui" ref="map"></div>
@@ -47,7 +47,13 @@
         data(){
             return{
                 address: "",
-                error:""
+                error:"",
+                lat:0,
+                long:0,
+                bbox0:0,
+                bbox1:0,
+                bbox2:0,
+                bbox3:0
             }
         },
     
@@ -56,7 +62,9 @@
                 if(navigator.geolocation){
                     navigator.geolocation.getCurrentPosition(position=>{
                         this.getAddressFrom(position.coords.latitude, position.coords.longitude)
-                        },
+                        this.lat=position.coords.latitude;
+                        this.long=position.coords.longitude;
+                    },
     
                     error=>{
                         this.error = error.message;
@@ -69,7 +77,7 @@
                 }
             },
             getAddressFrom(lat,long){
-                axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${long},${lat}.json?access_token=pk.eyJ1IjoiYnJycnJycmxhbCIsImEiOiJjbG9tZHpoMzAybTBjMmpuMHY3Nnh1YzI1In0.XaNXfY5h3jshsi9Vg1jnOA`)
+                axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${long},${lat}.json?access_token=pk.eyJ1IjoiYnJycnJycmxhbCIsImEiOiJjbG9tZTJyZWQyeTh0MnFuMG5nZXRtdnp1In0.7GaDEG5S6wXt8i7ZyM6PKA`)
                  .then(response => {
                     console.log(response.data);
                     if(response.data.error_message){
@@ -77,6 +85,10 @@
                     }else{
                             this.address =  response.data.features[0].place_name;
                             console.log(response.data.features[0].place_name);
+                            console.log(this.bbox0=response.data.features[2].bbox[0],
+                            this.bbox1=response.data.features[2].bbox[1],
+                            this.bbox2=response.data.features[2].bbox[2],
+                            this.bbox3=response.data.features[2].bbox[3])
                     }
                 })
                 .catch(error=>{
@@ -84,9 +96,17 @@
                 })
             },
             FindCloseByButtonPressed(){
-                // let URL=`https://api.mapbox.com/geocoding/v5/mapbox.places/coffee.json?proximity=${this.long},${this.lat}&bbox=${},${},${},${}&
-                // access_token=pk.eyJ1IjoiYnJycnJycmxhbCIsImEiOiJjbG9tZHpoMzAybTBjMmpuMHY3Nnh1YzI1In0.XaNXfY5h3jshsi9Vg1jnOA`
-                // axios.get(URL)
+                const URL=`https://api.mapbox.com/geocoding/v5/mapbox.places/restaurant.json?proximity=${this.long},${this.lat}
+                &bbox=${this.bbox0},${this.bbox1},${this.bbox2},${this.bbox3}
+                &access_token=pk.eyJ1IjoiYnJycnJycmxhbCIsImEiOiJjbG9tZTJyZWQyeTh0MnFuMG5nZXRtdnp1In0.7GaDEG5S6wXt8i7ZyM6PKA`
+                console.log(URL)
+                axios.get(URL)
+                .then(response=>{
+                    console.log(response.data);
+                })
+                .catch(error=>{
+                    console.log(error.message);
+                })
             }
         }
     };
